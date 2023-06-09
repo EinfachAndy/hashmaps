46a51efd74bd457c747efbf39021180a4fc77783
Andy Guenther <andy.guenther89@gmail.com>
2023-06-09 16:56:19 +0200

    [Added] implementation for a unordered hash map

--- map_test.go
+++ map_test.go
@@ -13,6 +13,39 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+func randString(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func setupMaps[K comparable, V comparable]() []hashmaps.IHashMap[K, V] {
+	robin := hashmaps.NewRobinHood[K, V]()
+	unordered := hashmaps.NewUnordered[K, V]()
+
+	return []hashmaps.IHashMap[K, V]{
+		{
+			Get:    unordered.Get,
+			Put:    unordered.Put,
+			Remove: unordered.Remove,
+			Size:   unordered.Size,
+			Each:   unordered.Each,
+			Load:   unordered.Load,
+		},
+		{
+			Get:    robin.Get,
+			Put:    robin.Put,
+			Remove: robin.Remove,
+			Size:   robin.Size,
+			Each:   robin.Each,
+			Load:   robin.Load,
+		},
+	}
+}
+
 func checkeq[K comparable, V comparable](cm *hashmaps.IHashMap[K, V], get func(k K) (V, bool), t *testing.T) {
 	cm.Each(func(key K, val V) bool {
 		if ov, ok := get(key); !ok {
@@ -31,22 +64,10 @@ func checkeq[K comparable, V comparable](cm *hashmaps.IHashMap[K, V], get func(k
 	})
 }
 
-func TestCrossCheck(t *testing.T) {
-	robin := hashmaps.NewRobinHood[uint64, uint32]()
+func TestCrossCheckInt(t *testing.T) {
 
-	maps := []hashmaps.IHashMap[uint64, uint32]{
-		{
-			Get:    robin.Get,
-			Put:    robin.Put,
-			Remove: robin.Remove,
-			Size:   robin.Size,
-			Each:   robin.Each,
-			Load:   robin.Load,
-		},
-	}
-
+	maps := setupMaps[uint64, uint32]()
 	const nops = 10000
-
 	for _, m := range maps {
 		stdm := make(map[uint64]uint32)
 		for i := 0; i < nops; i++ {
@@ -116,6 +137,80 @@ func TestCrossCheck(t *testing.T) {
 	}
 }
 
+func TestCrossCheckString(t *testing.T) {
+
+	maps := setupMaps[string, string]()
+	const nops = 1000
+	for i, m := range maps {
+		fmt.Println("test map:", i)
+		stdm := make(map[string]string)
+		for i := 0; i < nops; i++ {
+			key := randString(rand.Intn(40) + 10)
+			val := key
+			op := rand.Intn(4)
+
+			switch op {
+			case 0:
+				v1, ok1 := m.Get(key)
+				v2, ok2 := stdm[key]
+				if ok1 != ok2 || v1 != v2 {
+					t.Fatalf("lookup failed")
+				}
+			case 1:
+				// prioritize insert operation
+				fallthrough
+			case 2:
+				_, wasIn := stdm[key]
+				stdm[key] = val
+				isNew := m.Put(key, val)
+				if isNew == wasIn {
+					t.Fatalf("Put returned wrong state")
+				}
+
+				v, found := m.Get(key)
+				if !found {
+					t.Fatalf("lookup failed after insert for key %s", key)
+				}
+				if v != val {
+					t.Fatalf("values are not equal %s != %s", v, val)
+				}
+			case 3:
+				var del string
+				if len(stdm) == 0 {
+					break
+				}
+				for k := range stdm {
+					del = k
+					break
+				}
+				delete(stdm, del)
+
+				_, found := m.Get(del)
+				if !found {
+					t.Fatalf("lookup failed for key %s", key)
+				}
+				wasIn := m.Remove(del)
+				if !wasIn {
+					t.Fatalf("only deleted keys which are in")
+				}
+				_, found = m.Get(del)
+				if found {
+					t.Fatalf("key %s was not removed", key)
+				}
+			}
+
+			if len(stdm) != m.Size() {
+				t.Fatalf("len of maps are not equal %d != %d", len(stdm), m.Size())
+			}
+
+			checkeq(&m, func(k string) (string, bool) {
+				v, ok := stdm[k]
+				return v, ok
+			}, t)
+		}
+	}
+}
+
 func TestCopy(t *testing.T) {
 	orig := hashmaps.NewRobinHood[uint64, uint32]()
 
@@ -176,9 +271,42 @@ func TestComplexKeyType(t *testing.T) {
 	hasher := func(d dummy) uintptr {
 		return 0
 	}
-	m := hashmaps.NewRobinHoodWithHasher[dummy, uint32](hasher)
-	m.Put(dummy{a: 0, b: 0, c: "", d: 0, e: 0}, 0)
-	if m.Size() != 1 {
-		t.Fatal("could not insert elem")
+	robin := hashmaps.NewRobinHoodWithHasher[dummy, string](hasher)
+	unordered := hashmaps.NewUnorderedWithHasher[dummy, string](hasher)
+	maps := []hashmaps.IHashMap[dummy, string]{
+		{
+			Get:    robin.Get,
+			Put:    robin.Put,
+			Remove: robin.Remove,
+			Size:   robin.Size,
+			Each:   robin.Each,
+			Load:   robin.Load,
+		},
+		{
+			Get:    unordered.Get,
+			Put:    unordered.Put,
+			Remove: unordered.Remove,
+			Size:   unordered.Size,
+			Each:   unordered.Each,
+			Load:   unordered.Load,
+		},
+	}
+
+	for _, m := range maps {
+
+		isNew := m.Put(dummy{a: 0, b: 0, c: "test", d: 0, e: 0}, "xxx")
+		if m.Size() != 1 || !isNew {
+			t.Fatal("could not insert elem")
+		}
+
+		val, found := m.Get(dummy{a: 0, b: 0, c: "test", d: 0, e: 0})
+		if !found || val != "xxx" {
+			t.Fatal("lookup failed, elem missed")
+		}
+
+		_, found = m.Get(dummy{a: 0, b: 0, c: "test1", d: 0, e: 0})
+		if found {
+			t.Fatal("lookup failed, unexpected elem")
+		}
 	}
 }
---
+++ unordered.go
@@ -0,0 +1,200 @@
+package hashmaps
+
+type linkedList[K comparable, V any] struct {
+	head *listNode[K, V]
+}
+
+type listNode[K comparable, V any] struct {
+	next  *listNode[K, V]
+	key   K
+	value V
+}
+
+// Unordered is a hash map implementation, where the elements are organized into buckets
+// depending on their hash values. Collisions are chained in a single linked list.
+// An inserted value keeps its memory address, means a element in a bucket will not copied
+// or swapped. That supports holding points instead of copy by value. see: `Insert` and `lookup`.
+type Unordered[K comparable, V any] struct {
+	buckets []linkedList[K, V]
+	hasher  HashFn[K]
+	// length stores the current inserted elements
+	length uintptr
+	// capMinus1 is used for a bitwise AND on the hash value,
+	// because the size of the underlying array is a power of two value
+	capMinus1 uintptr
+}
+
+// NewUnordered creates a ready to use `unordered` hash map with default settings.
+func NewUnordered[K comparable, V any]() *Unordered[K, V] {
+	return NewUnorderedWithHasher[K, V](GetHasher[K]())
+}
+
+// NewUnorderedWithHasher same as `NewUnordered` but with a given hash function.
+func NewUnorderedWithHasher[K comparable, V any](hasher HashFn[K]) *Unordered[K, V] {
+	return &Unordered[K, V]{
+		capMinus1: 3,
+		buckets:   make([]linkedList[K, V], 4),
+		hasher:    hasher,
+	}
+}
+
+// Get returns the value stored for this key, or false if not found.
+func (m *Unordered[K, V]) Get(key K) (V, bool) {
+	idx := m.hasher(key) & m.capMinus1
+	for current := m.buckets[idx].head; current != nil; current = current.next {
+		if current.key == key {
+			return current.value, true
+		}
+	}
+	var v V
+	return v, false
+}
+
+// Lookup returns a pointer to the stored value for this key or nil if not found.
+// The pointer is valid until the key is part of the hash map.
+// Note, use `Get` for small values.
+func (m *Unordered[K, V]) Lookup(key K) *V {
+	idx := m.hasher(key) & m.capMinus1
+	for current := m.buckets[idx].head; current != nil; current = current.next {
+		if current.key == key {
+			return &(current.value)
+		}
+	}
+	return nil
+}
+
+// Insert returns a pointer to a zero allocated value. These pointer is valid until
+// the key is part of the hash map. Note, use `Put` for small values.
+func (m *Unordered[K, V]) Insert(key K) (*V, bool) {
+	if m.length >= uintptr(cap(m.buckets)) {
+		m.grow()
+	}
+
+	idx := m.hasher(key) & m.capMinus1
+	// check head
+	if m.buckets[idx].head == nil {
+		newNode := &listNode[K, V]{key: key}
+		m.buckets[idx].head = newNode
+		m.length++
+		return &newNode.value, true
+	}
+
+	// search
+	for current := m.buckets[idx].head; ; current = current.next {
+		if current.key == key {
+			return &current.value, false
+		}
+		// reached end of list, so insert
+		if current.next == nil {
+			m.length++
+			newNode := &listNode[K, V]{key: key}
+			current.next = newNode
+			return &newNode.value, true
+		}
+	}
+}
+
+func (m *Unordered[K, V]) rehash(n uintptr) {
+	m.capMinus1 = n - 1
+	newBuckets := make([]linkedList[K, V], n)
+	for _, l := range m.buckets {
+		for current := l.head; current != nil; {
+			newElem := current
+			current = current.next
+			newElem.next = nil // unlink from old
+
+			// push newElem to front of the list
+			newIdx := m.hasher(newElem.key) & m.capMinus1
+			head := newBuckets[newIdx].head
+			headRef := &newBuckets[newIdx].head
+			*headRef = newElem
+			newElem.next = head
+		}
+	}
+	m.buckets = newBuckets
+}
+
+// Clear removes all key-value pairs from the map.
+func (m *Unordered[K, V]) Clear() {
+	for idx := range m.buckets {
+		m.buckets[idx].head = nil
+	}
+}
+
+// Size returns the number of items in the map.
+func (m *Unordered[K, V]) Size() int {
+	return int(m.length)
+}
+
+// Load return the current load of the hash map.
+func (m *Unordered[K, V]) Load() float32 {
+	return float32(m.length) / float32(len(m.buckets))
+}
+
+func (m *Unordered[K, V]) grow() {
+	newSize := uintptr(cap(m.buckets) * 2)
+	m.rehash(newSize)
+}
+
+// Reserve sets the number of buckets to the most appropriate to contain at least n elements.
+// If n is lower than that, the function may have no effect.
+func (m *Unordered[K, V]) Reserve(n uintptr) {
+	newCap := uintptr(NextPowerOf2(uint64(n)))
+	if uintptr(cap(m.buckets)) < newCap {
+		m.rehash(newCap)
+	}
+}
+
+// Put maps the given key to the given value. If the key already exists its
+// value will be overwritten with the new value.
+// Returns true, if the element is a new item in the hash map.
+// go:inline
+func (m *Unordered[K, V]) Put(key K, val V) bool {
+	v, isNew := m.Insert(key)
+	*v = val
+	return isNew
+}
+
+// Remove removes the specified key-value pair from the map.
+// Returns true, if the element was in the hash map.
+func (m *Unordered[K, V]) Remove(key K) bool {
+	var (
+		idx                     = m.hasher(key) & m.capMinus1
+		current                 = m.buckets[idx].head
+		prev    *listNode[K, V] = nil
+	)
+
+	// check head
+	if current != nil && current.key == key {
+		m.buckets[idx].head = current.next
+		m.length--
+		return true
+	}
+
+	// search for the key
+	for current != nil && current.key != key {
+		prev = current
+		current = current.next
+	}
+	if current == nil {
+		return false // not found
+	}
+	// unlink
+	prev.next = current.next
+	m.length--
+	return true
+
+}
+
+// Each calls 'fn' on every key-value pair in the hash map in no particular order.
+// If 'fn' returns true, the iteration stops.
+func (m *Unordered[K, V]) Each(fn func(key K, val V) bool) {
+	for _, l := range m.buckets {
+		for current := l.head; current != nil; current = current.next {
+			if stop := fn(current.key, current.value); stop {
+				// stop iteration
+				return
+			}
+		}
+	}
+}
